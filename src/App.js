767aa35cd9a0989cf8af847e01c0cc4e3174e8c5
yasvipatel52 <yasvi.patel@marutitech.com>
2023-02-02 18:29:28 +0530

    useState done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{ useState } from 'react';
 
 function App() {
+
+function initialCounter(){
+  console.log("initial counter called")
+  return 3;
+}
+//  const [counter,setCounter]=useState({count:4,theme:'blue'});
+// const [counter,setCounter]=useState(()=>initialCounter());
+const [counter,setCounter]=useState(1);
+const [counterArray,setCounterArray]=useState([1,2,3]);
+
+function counterChange(){
+   setCounterArray(prevState=>[...prevState,4])
+}
+
+ function decrement(){
+   setCounter(prevState=>prevState-1)
+ }
+ function increment(){
+  setCounter(prevState=>prevState+1)
+ }
+// function decrement(){
+//      setCounter(prevState=>{
+//         return{...prevState,count:prevState.count-1};
+//      })
+//    }
+// function increment(){
+//     setCounter(prevState=>{
+//         return{...prevState,count:prevState.count+1};
+//     })
+//    }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <button onClick={decrement}>-</button>
+     {/* <span>{counter.count}</span>
+     <span>{counter.theme}</span> */}
+     <span>{counter}</span>
+     <button onClick={increment}>+</button>
+     <br/>
+     
+     <button onClick={counterChange}>Array</button>
+     <span>{counterArray}</span>
+    </>
   );
 }
 
